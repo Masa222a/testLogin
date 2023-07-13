6571a87f9f32b6a372fd303bae3cef356d3a1446
Masa222a <guitarstring0716@gmail.com>
2023-07-13 17:44:04 +0900

    RegisterActivityのレイアウトを作成,Firebase Authの導入

--- app/src/main/java/com/hannibal/testlogin/RegisterActivity.kt
+++ app/src/main/java/com/hannibal/testlogin/RegisterActivity.kt
@@ -1,11 +1,19 @@
 package com.hannibal.testlogin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val loginText = findViewById<TextView>(R.id.textView_login_now)
+        loginText.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
