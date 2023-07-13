70faab500f9a77a0add4a11247fa63177a11f064
Masa222a <guitarstring0716@gmail.com>
2023-07-13 18:21:45 +0900

    メールアドレス、パスワードでログイン認証機能追加

--- app/src/main/java/com/hannibal/testlogin/LoginActivity.kt
+++ app/src/main/java/com/hannibal/testlogin/LoginActivity.kt
@@ -3,12 +3,23 @@ package com.hannibal.testlogin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = Firebase.auth
 
         val registertext = findViewById<TextView>(R.id.textView_register_now)
 
@@ -16,5 +27,59 @@ class LoginActivity : AppCompatActivity() {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
+
+        val loginButton = findViewById<Button>(R.id.button_login)
+
+        loginButton.setOnClickListener {
+            performLogin()
+        }
+
+        //let do login now.
+        //first we add a login button
+
+    }
+
+    private fun performLogin() {
+        //lets get input from the user
+        val email = findViewById<EditText>(R.id.editText_email_login)
+        val password = findViewById<EditText>(R.id.editText_password_login)
+
+        // null checks on inputs
+        if (email.text.isEmpty() || password.text.isEmpty()) {
+            Toast.makeText(this, "Please fill all the fields", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val emailInput = email.text.toString()
+        val passwordInput = password.text.toString()
+
+        auth.signInWithEmailAndPassword(emailInput, passwordInput)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, navigate to the Main Activity
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+
+                    Toast.makeText(
+                        baseContext,
+                        "Success.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
+            .addOnFailureListener {
+                Toast.makeText(
+                    baseContext,
+                    "Authentication failed. ${it.localizedMessage}",
+                    Toast.LENGTH_SHORT,
+                ).show()
+            }
     }
 }
