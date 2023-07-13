25f330d392a3b030a16d9013897cf4257e180e10
Masa222a <guitarstring0716@gmail.com>
2023-07-13 17:39:44 +0900

    LoginActivityのレイアウトを作成

--- app/src/main/java/com/hannibal/testlogin/LoginActivity.kt
+++ app/src/main/java/com/hannibal/testlogin/LoginActivity.kt
@@ -1,11 +1,20 @@
 package com.hannibal.testlogin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val registertext = findViewById<TextView>(R.id.textView_register_now)
+
+        registertext.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
